4e581daaaa3d272554ff8caafc3c1cf56c9cdbc8
Tony Dandino <tdandino1005@gmail.com>
2022-11-13 08:26:04 -0600

    saved work

--- Develop/script.js
+++ Develop/script.js
@@ -1,60 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
-
-
-    //Assign saveBtn click listener for user input and time stamp??
-    $(".saveBtn").on("click", function () {
-        //get nearby values.
-        console.log(this);
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        //set items in local storage.
-        localStorage.setItem(time, text);
-    })
-    //load any saved data from LocalStorage - do this for each hour created.
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-
-
-    function hourTracker() {
-        //get current number of hours.
-        var currentHour = moment().hour();
-
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-            console.log( blockHour, currentHour)
-
-            //check if we've moved past this time
-            if (blockHour < currentHour) {
-                $(this).addClass("past");
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-            }
-            else if (blockHour === currentHour) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-                $(this).removeClass("future");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-            }
-        })
-    }
-    hourTracker();
-})
+ // Display today's day and date
+ let todayDate = moment().format('dddd, MMM Do YYYY');
+ $("#currentDay").html(todayDate);
 
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -74,6 +23,4 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  // Display today's day and date
-  let todayDate = moment().format('dddd, MMM Do YYYY');
-  $("#currentDay").html(todayDate);
+ 
