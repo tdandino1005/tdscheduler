84be93d2e68dc1bf14ebfe8314ab5138b9107b2a
Tony Dandino <tdandino1005@gmail.com>
2022-11-13 22:22:31 -0600

    Added coded in Js

--- Develop/script.js
+++ Develop/script.js
@@ -21,4 +21,56 @@
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
- 
+ var now = day.js()
+ var timeblock1 = document.querySelector('.container');
+
+ $('#currentDay').text(today.format('LLL'));
+ let current = new Date();
+let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+let dateTime = cDate + ' ' + cTime;
+console.log(dateTime);
+
+$('saveBtn').on('click', funtion() {
+    var textvalue = $(this).siblings('.description').val();
+    var timeKey = $(this).parent().attr('id');
+    localStorage.setItem(timeKey, textvalue);
+});
+
+$('#hour8 .description').val(localStorage.getItem('hour8'));
+$('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+
+function audit() {
+    let currentHour = today.hours();
+
+    $('.time-block').each(function () {
+        var time = parseInt($(this).attr('id').split("hour")[1]);
+
+        if (time < currentHour) {
+            $(this).addClass('past');
+
+            else if (time === currentHour) {
+                $(this).removeClass('past');
+                $(this).removeClass('future');
+                $(this).addClass('present');
+            }
+            else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+    });
+}
+
+audit();
+
+setTimeout(function ()  {
+    location = ''
+}, 1000 * 60);
