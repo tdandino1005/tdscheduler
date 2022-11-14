2ba7ef91736c7926f7cbca15480209d95d9a5f46
Tony Dandino <tdandino1005@gmail.com>
2022-11-13 23:25:29 -0600

    added timeblock

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,20 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
- // Display today's day and date
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
- var now = day.js()
+ var today = moment();
  var timeblock1 = document.querySelector('.container');
 
  $('#currentDay').text(today.format('LLL'));
- let current = new Date();
-let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
-let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
-let dateTime = cDate + ' ' + cTime;
-console.log(dateTime);
 
-$('saveBtn').on('click', funtion() {
-    var textvalue = $(this).siblings('.description').val();
+$('.saveBtn').on('click', function () {
+
+    var textValue = $(this).siblings('.description').val();
+
     var timeKey = $(this).parent().attr('id');
-    localStorage.setItem(timeKey, textvalue);
-});
+  
+    localStorage.setItem(timeKey, textValue);
+  });
+
+
+
 
 $('#hour8 .description').val(localStorage.getItem('hour8'));
 $('#hour9 .description').val(localStorage.getItem('hour9'));
@@ -55,6 +34,7 @@ function audit() {
 
         if (time < currentHour) {
             $(this).addClass('past');
+        }
 
             else if (time === currentHour) {
                 $(this).removeClass('past');
